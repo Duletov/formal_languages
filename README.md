44130cedd0c0762cd8d5d6b1a6ccc2ff32cc1fe0
Duletov <duletov.dmitriy@gmail.com>
2020-09-16 18:11:28 +0300

    Minor fixes

--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 
 [![Build Status](https://travis-ci.com/Duletov/formal_languages.svg?branch=master)](https://travis-ci.com/Duletov/formal_languages)
 [![Build Status](https://travis-ci.com/Duletov/formal_languages.svg?branch=Task01)](https://travis-ci.com/Duletov/formal_languages)
+[![Build Status](https://travis-ci.com/Duletov/formal_languages.svg?branch=Task02)](https://travis-ci.com/Duletov/formal_languages)
 
-#### Installation & Running tests
+#### Installation & Running
 
- - The only requirement for running tests is Docker:
-   - `docker build ./ -t tests`
-   - `docker run tests`
+ - The only requirement for running is Docker:
+   - `docker build ./ -t formal_languages --build-arg graph=<path to graph file> --build-arg regex=<path to regex file> [--build-arg start=<path to the file with start vertices>] [--build-arg end=<path to the file with end vertices>]`
+   - `docker run formal_languages`
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from Graph import Graph
 from pygraphblas import *
 import argparse
 
+
 def intersect(graph_1, graph_2):
     result = Graph()
     result.n_vertices = graph_1.n_vertices * graph_2.n_vertices
@@ -17,55 +18,57 @@ def intersect(graph_1, graph_2):
         result.label_matrices[label] = graph_1.get_by_label(label).kronecker(graph_2.get_by_label(label))
 
     return result
-        
+
+
 def output(graph):
     for label in graph.labels():
-        print (label)
-        print(graph.get_by_label(label).nvals)
+        print(label, graph.get_by_label(label).nvals)
+
 
 def all_path(graph):
     adj = graph.transitive_closure()
     adj.select(lib.GxB_NONZERO)
     for i, j, _ in zip(*adj.to_lists()):
         print(f'from {i} to {j}')
-        
+
+
 def from_bunch(graph, filename):
     input_file = open(filename)
     vertices = input_file.read().rstrip().split('\n')
     input_file.close()
     result = [int(item) for item in vertices]
-    print(result)
-    
+
     adj = graph.transitive_closure()
     adj.select(lib.GxB_NONZERO)
     for i, j, _ in zip(*adj.to_lists()):
         if i in result:
             print(f'from {i} to {j}')
-            
+
+
 def from_bunch_to_bunch(graph, filename_1, filename_2):
     input_file = open(filename_1)
     vertices = input_file.read().rstrip().split('\n')
     input_file.close()
     result_1 = [int(item) for item in vertices]
-    
+
     input_file = open(filename_2)
     vertices = input_file.read().rstrip().split('\n')
     input_file.close()
     result_2 = [int(item) for item in vertices]
-    
-    
+
     adj = graph.transitive_closure()
     adj.select(lib.GxB_NONZERO)
     for i, j, _ in zip(*adj.to_lists()):
         if (i in result_1) and (j in result_2):
             print(f'from {i} to {j}')
 
+
 def main():
     g = Graph()
     g.from_trans(args.graph)
     h = Graph()
     h.from_regex(args.regex)
-    
+
     inter = intersect(g, h)
     output(inter)
     if args.start and args.end:
@@ -79,29 +82,29 @@ def main():
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='command line interface for simple graph/dfa operations')
     parser.add_argument(
-        '--graph'
-        , required=True
-        , type=str
-        , help='path to the graph file'
+        '--graph',
+        required=True,
+        type=str,
+        help='path to the graph file',
     )
     parser.add_argument(
-        '--regex'
-        , required=True
-        , type=str
-        , help='path to the regex file'
+        '--regex',
+        required=True,
+        type=str,
+        help='path to the regex file',
     )
     parser.add_argument(
-        '--start'
-        , required=False
-        , type=str
-        , help='path to the file with start vertices'
+        '--start',
+        required=False,
+        type=str,
+        help='path to the file with start vertices',
     )
     parser.add_argument(
-        '--end'
-        , required=False
-        , type=str
-        , help='path to the file with end vertices'
+        '--end',
+        required=False,
+        type=str,
+        help='path to the file with end vertices',
     )
     args = parser.parse_args()
-    
+
     main()
