d80bf832d36c7b2fd4b3b7f335a7ddd9a6ce7a98
Duletov <duletov.dmitriy@gmail.com>
2020-09-23 00:52:27 +0300

    Task03

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from Graph import Graph
 from pygraphblas import *
 import argparse
+import time
+import copy
 
+res_trans1 = list()
+res_trans2 = list()
+res_pairs = list()
+res = list()
 
 def intersect(graph_1, graph_2):
     result = Graph()
@@ -42,22 +48,75 @@ def call(graph, filename_1, filename_2):
         input_file.close()
         result_2 = [int(item) for item in vertices]
 
-    adj = graph.transitive_closure()
-    adj.select(lib.GxB_NONZERO)
-    for i, j, _ in zip(*adj.to_lists()):
-        if (i in result_1) and (j in result_2):
-            print(f'from {i} to {j}')
+    time3 = 0
+    for i in range(5):
+            time1 = time.time()
+            adj = graph.transitive_closure_1()
+            time2 = time.time()
+            time3 += time2 - time1
+    print(adj.nvals)
+    print(f"Time to transitive_closure_1 {time3/5}")
+    res_trans1.append(time3/5)
+    
+    res.append(adj.nvals)
+    
+    for i in range(5):
+            time1 = time.time()
+            adj = graph.transitive_closure_2()
+            time2 = time.time()
+            time3 += time2 - time1
+    print(adj.nvals)
+    print(f"Time to transitive_closure_2 {time3/5}")
+    res_trans2.append(time3/5)
+    
+    for i in range(5):
+            time1 = time.time()
+            adj.select(lib.GxB_NONZERO)
+            for i, j, _ in zip(*adj.to_lists()):
+                if (i in result_1) and (j in result_2):
+                    print(f'from {i} to {j}')
+            time2 = time.time()
+            time3 += time2 - time1
+    print(adj.nvals)
+    print(f"Time to calculate pairs {time3/5}")
+    res_pairs.append(time3/5)
 
 
 def main():
+    time1 = time.time()
     g = Graph()
     g.from_trans(args.graph)
-    h = Graph()
-    h.from_regex(args.regex)
-
-    inter = intersect(g, h)
-    output(inter)
-    call(inter, args.start, args.end)
+    time2 = time.time()
+    print(f"Time to built graph {time2 - time1}")
+    
+    input_file = open(args.regex)
+    regexes = input_file.read().rstrip().split('\n')
+    input_file.close()
+    
+    res_inter = list()
+    
+    for file in regexes:
+        print()
+        print(file)
+        h = Graph()
+        h.from_regex(file)
+        time3 = 0
+        for i in range(5):
+            time1 = time.time()
+            inter = intersect(g, h)
+            time2 = time.time()
+            time3 += time2 - time1
+        print(f"Time to intersect {time3/5}")
+        res_inter.append(time3/5)
+        
+        output(inter)
+        call(inter, args.start, args.end)
+        
+    '''print(res_inter)
+    print(res_trans1)
+    print(res_trans2)
+    print(res_pairs)
+    print(res)'''
 
 
 if __name__ == "__main__":
@@ -72,7 +131,7 @@ if __name__ == "__main__":
         '--regex',
         required=True,
         type=str,
-        help='path to the regex file',
+        help='path to file with names of regex files',
     )
     parser.add_argument(
         '--start',
